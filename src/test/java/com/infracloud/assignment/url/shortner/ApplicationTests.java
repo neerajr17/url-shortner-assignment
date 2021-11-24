810d2caa776cf236810f14dc821cccac2dae9f16
Neeraj R <neerajcfc17@gmail.com>
2021-11-24 11:48:03 +0530

    Initial commit: Initial commit of spring boot application created using Spring Initializer

---
+++ src/test/java/com/infracloud/assignment/url/shortner/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infracloud.assignment.url.shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
